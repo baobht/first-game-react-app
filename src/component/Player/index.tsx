e57a65b8264b36ffd5c769495f0cb566c28e8361
Hai KhuonG <khuong.nguyen@itbeesolutions.vn>
2022-10-27 18:45:19 +0700

    .

--- src/component/Player/index.tsx
+++ src/component/Player/index.tsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from "react";
 
 // import "./styles.scss";
 
-function draw(_gameWidth: any, _gameHeight: any) {
-  let gameWidth = _gameWidth;
-  let gameHeight = _gameHeight;
-  let states: any[] = [];
-  let currentState = states[0];
-  let image = document.getElementById("dog");
-  let width = 200;
-  let height = 181.83;
-  let x = 0;
-  let y = 0;
+export const draw = (canvas: CanvasRenderingContext2D | null, _gameWidth: number, _gameHeight: number, key: number, curFrame: number) => {
+  // console.log("🚀 ~ file: index.tsx ~ line 5 ~ draw ~ curFrame", curFrame)
+  const gameWidth = _gameWidth;
+  const gameHeight = _gameHeight;
+  const states: any[] = [];
+  const currentState = states[0];
+  const image = document.getElementById("dog");
+  const width = 200;
+  const height = 181.83;
+  const maxFrames = 6;
+  const x = gameWidth / 2 - width / 2;
+  const y = gameHeight - height - 101;
+  let frameX = curFrame
+  const frameY = key
+  if (canvas) {
+    canvas.drawImage(image as CanvasImageSource, width * frameX, height * frameY, width, height, x, y, width, height)
 
-//   return context.drawImage(image, x, y);
+  }
 }
-export default draw;
+
+export const convertKey = (key: { prev: string, next: string }) => {
+  const { prev, next } = key
+  if (next.includes("LEFT")) return "1";
+  return "0"
+}
+
+export const drawStatusText = (canvas: CanvasRenderingContext2D | null, lastKey: string) => {
+  if (canvas) {
+    canvas.font = '30px Helvetica';
+    canvas.fillText('Last input: ' + lastKey, 20, 50)
+  }
+}
+
--- src/component/testComp.tsx
+++ src/component/testComp.tsx
@@ -1,23 +1,122 @@
-import React, { useEffect } from "react";
-import logo from "../logo.svg";
+import { useEffect, useState } from "react";
 import dog from "../assets/dog_left_right_white.png";
 
+import { convertKey, draw, drawStatusText } from "./Player";
 import "./styles.scss";
-import Player from "./Player";
 
 function NewComponent() {
-    useEffect(() => {
-        window.addEventListener('load', () => {
-            const player = new (Player as any)(window.innerWidth, window.innerHeight);
-            console.log("🚀 ~ file: testComp.tsx ~ line 12 ~ window.addEventListener ~ player", player)
-        })
-    }, [])
+  const curFrame = 0
+  const [click, setCLick] = useState(false);
+  const [lastKey, setLastKey] = useState({
+    prev: "",
+    next: "",
+  })
+  useEffect(() => {
+    // window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1') as HTMLCanvasElement
+    const ctx = canvas.getContext('2d')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    let frameXXX = 0;
+
+    const a = setInterval(async () => {
+      const key = Number(localStorage.getItem('lastKey') || 0)
+      if (frameXXX < 6) {
+        ctx?.clearRect(0, 0, canvas.width, canvas.height)
+        draw(ctx, canvas.width, canvas.height, key, frameXXX)
+        frameXXX++;
+      }
+      else frameXXX = 0;
+
+    }, 120)
+    console.log(a);
+
+    console.log("🚀 ~ file: testComp.tsx ~ line 12 ~ window.addEventListener ~ player")
+    // })
+    return () => {
+      clearInterval(a);
+      console.log(canvas);
+      const key = Number(localStorage.getItem('lastKey') || 0)
+      draw(ctx, canvas.width, canvas.height, key, frameXXX)
+
+    }
+  }, [click, lastKey])
+
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      console.log("vốdisdoi");
+
+      // if (e.key !== lastKey.next) {
+      switch (e.key) {
+        case 'ArrowLeft':
+          setLastKey({
+            prev: lastKey.next,
+            next: "PRESS_LEFT"
+          })
+          localStorage.setItem('lastKey', (convertKey({
+            prev: lastKey.next,
+            next: "PRESS_LEFT"
+          })))
+          break;
+        case 'ArrowRight':
+          setLastKey({
+            prev: lastKey.next,
+            next: "PRESS_RIGHT"
+          })
+          localStorage.setItem('lastKey', (convertKey({
+            prev: lastKey.next,
+            next: "PRESS_RIGHT"
+          })))
+          break;
+        case 'ArrowDown':
+          setLastKey({
+            prev: lastKey.next,
+            next: "PRESS_DOWN"
+          })
+
+          localStorage.setItem('lastKey', convertKey({
+            prev: lastKey.next,
+            next: "PRESS_DOWN"
+          }))
+          break;
+        case 'ArrowUp':
+          setLastKey({
+            prev: lastKey.next,
+            next: "PRESS_UP"
+          })
+
+          localStorage.setItem('lastKey', (convertKey({
+            prev: lastKey.next,
+            next: "PRESS_UP"
+          })))
+          break;
+        default:
+          break;
+      }
+      // }
+    })
+
+    // window.addEventListener('keyup', (e) => {
+    //   console.log(e);
+    //   switch (e.key) {
+    //     case 'ArrowLeft':
+    //       setLastKey('PRESS left')
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // })
+  }, [])
+
   return (
     <div className="Game">
+      <button onClick={() => {
+
+        setCLick(!click);
+      }}>Hello</button>
       <canvas id="canvas1"></canvas>
       <img src={dog} id="dog" className="Game-logo" alt="logo" />
-      <h1 id="loading">Loading....</h1>
-      
+      {/* <h1 id="loading">Loading....</h1> */}
     </div>
   );
 }
